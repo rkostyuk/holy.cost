fa13454304fcb606b06fda71346d79fe0e412a2e
Roman Kostyuk <kostyuk.roman@gmail.com>
2014-05-06 09:52:12 +0500

    add project holy-cost

---
+++ holy-cost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  holy-cost
+//
+//  Created by Roman Kostyuk on 5/6/14.
+//  Copyright (c) 2014 Roman Kostyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ holy-costTests/holy_costTests.m
@@ -0,0 +1,34 @@
+//
+//  holy_costTests.m
+//  holy-costTests
+//
+//  Created by Roman Kostyuk on 5/6/14.
+//  Copyright (c) 2014 Roman Kostyuk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface holy_costTests : XCTestCase
+
+@end
+
+@implementation holy_costTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
